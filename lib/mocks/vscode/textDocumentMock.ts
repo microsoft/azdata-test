1057d46bc0ca444ed811cf0a2a60206d37842a96
Charles Gagnon <chgagnon@microsoft.com>
2022-07-05 16:28:22 -0700

    add comment

--- lib/mocks/vscode/textDocumentMock.ts
+++ lib/mocks/vscode/textDocumentMock.ts
@@ -8,6 +8,12 @@ import * as TypeMoq from 'typemoq';
 
 export type RangeWithContent = { range: vscode.Range, content: string };
 
+/**
+ * Creates a mock of a vscode.TextDocument 
+ * @param uri The URI to give the document
+ * @param contentOrRanges The content of the document. If just a string then that is returned when all context is gotten from the document, otherwise the array is a set of range to content mappings to return when that specified range is requested
+ * @returns The mocked document
+ */
 export function createTextDocumentMock(uri: vscode.Uri, contentOrRanges: string | RangeWithContent[] = ''): TypeMoq.IMock<vscode.TextDocument> {
 	const mock = TypeMoq.Mock.ofType<vscode.TextDocument>();
 	mock.setup(m => m.uri).returns(() => uri);
