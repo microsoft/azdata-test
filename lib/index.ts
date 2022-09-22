523466ca81093b7b99428eda6deda48d1976d539
Alan Ren <alanren@microsoft.com>
2022-09-21 19:44:16 -0700

    format

--- lib/index.ts
+++ lib/index.ts
@@ -9,4 +9,4 @@ export { resolveCliPathFromAzureDataStudioExecutablePath } from './util';
 export * as stubs from './stubs';
 export * as mocks from './mocks';
 export * as helpers from './helpers';
-export {getDefaultMochaOptions} from './testOptions';
+export { getDefaultMochaOptions } from './testOptions';
